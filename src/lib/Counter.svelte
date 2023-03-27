6b0ea9d83e5ae663abe8ecf3c2a16fa57928fadf
Sidd <psiddhu001@gmail.com>
2023-03-27 18:59:20 -0400

    Finished setup

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,10 @@
 <script>
-  let count = 0
+  let count = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
 <button on:click={increment}>
-  count is {count}
+  Count is {count}
 </button>
